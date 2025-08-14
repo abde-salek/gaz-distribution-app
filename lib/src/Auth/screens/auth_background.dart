568440069538d9fdf53a8fec8f81fc8da51260c0
Abdelghani Salek <a.salek@edu.umi.ac.ma>
2025-08-15 00:18:09 +0100

    Day 28 Implement AuthBackground

---
+++ lib/src/Auth/screens/auth_background.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:gaz/Core/responsive_ui.dart';
+import '../../../core/app_colors.dart'; // Import your colors
+
+class AuthBackground extends StatelessWidget {
+  const AuthBackground({super.key, this.onArrowPressed});
+
+  final VoidCallback? onArrowPressed;
+
+  // ignore: non_constant_identifier_names
+  Widget TopCircleText({required String text, required double screenWidth}) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontFamily: 'Futura Hv BT',
+        color: Colors.white,
+        fontSize: screenWidth * 0.075,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class TopCircleText extends StatelessWidget {
+  final String text;
+  final double screenWidth;
+
+  const TopCircleText({Key? key, required this.text, required this.screenWidth})
+    : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontFamily: 'Futura Hv BT',
+        color: Colors.white,
+        fontSize: screenWidth * 0.075,
+      ),
+    );
+  }
+}
+
+class BottomCircleArrow extends StatelessWidget {
+  final VoidCallback onPressed;
+  final double screenWidth;
+
+  const BottomCircleArrow({
+    Key? key,
+    required this.onPressed,
+    required this.screenWidth,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: EdgeInsets.all(screenWidth * 0.88 * 0.02),
+        decoration: const BoxDecoration(shape: BoxShape.circle),
+        child: IconButton(
+          icon: const Icon(Icons.arrow_forward, color: Colors.white, size: 35),
+          onPressed: onPressed,
+        ),
+      ),
+    );
+  }
+}

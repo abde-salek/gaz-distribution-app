9de6dffffd575d6fdc0ea1c4aa30050193e1ef89
Abdelghani Salek <a.salek@edu.umi.ac.ma>
2025-08-23 12:09:39 +0100

    Day 35 Dashboard screen with navigation

--- lib/src/Auth/screens/auth_background.dart
+++ lib/src/Auth/screens/auth_background.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gaz/Core/Dash.dart';
+import 'package:gaz/src/Dashboard/screens/dashboard_screen.dart';
 import 'package:gaz/Core/app_colors.dart';
 import 'package:gaz/Core/responsive_ui.dart';
 
--- lib/src/Dashboard/screens/dashboard_screen.dart
+++ lib/src/Dashboard/screens/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gaz/widgets/app_navigation_bar.dart';
 
 class Dash extends StatefulWidget {
   const Dash({super.key});
@@ -8,8 +9,23 @@ class Dash extends StatefulWidget {
 }
 
 class _DashState extends State<Dash> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    // Add navigation logic here
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(child: Text('You are on page $_selectedIndex')),
+      bottomNavigationBar: AppNavigationBar(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
+      ),
+    );
   }
 }

e1dd698e6ae4fffc55829c912a92463bff5c50b3
Abdelghani Salek <a.salek@edu.umi.ac.ma>
2025-08-30 14:40:18 +0100

    Day 38.1 Create app_bar as reusable component

--- lib/src/Dashboard/screens/dashboard_screen.dart
+++ lib/src/Dashboard/screens/dashboard_screen.dart
@@ -21,10 +21,7 @@ class _DashState extends State<Dash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        
-        title: const Text('Dashboard'),
-      ),
+      
       body: Center(child: Text('You are on page $_selectedIndex')),
       bottomNavigationBar: BottomNavBar(
         selectedIndex: _selectedIndex,

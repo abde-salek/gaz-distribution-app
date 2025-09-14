ef2d631bf93087747b85bb8e667deecdc95895d6
Abdelghani Salek <a.salek@edu.umi.ac.ma>
2025-09-14 15:35:34 +0100

    Day 48.1 Converting General Design into UI: client details screen

--- lib/src/clients/screens/client_details_screen.dart
+++ lib/src/clients/screens/client_details_screen.dart
@@ -1,5 +1,9 @@
 // Client details screen to display the details of a client.//
 import 'package:flutter/material.dart';
+import 'package:gaz/widgets/currency_switcher.dart';
+import 'package:gaz/widgets/app_bar.dart';
+import 'package:gaz/widgets/app_navigation_bar.dart';
+
 
 class ClientDetailsScreen extends StatefulWidget {
   const ClientDetailsScreen({super.key});
@@ -8,6 +12,7 @@ class ClientDetailsScreen extends StatefulWidget {
   State<ClientDetailsScreen> createState() => _ClientDetailsScreenState();
 }
 
+// Client details screen state.//
 class _ClientDetailsScreenState extends State<ClientDetailsScreen> {
   @override
   Widget build(BuildContext context) {
@@ -38,7 +43,6 @@ class _ClientDetailsScreenState extends State<ClientDetailsScreen> {
                         ),
                       ),
                     ),
-                    BottomNavigation(),
                   ],
                 ),
               ),
@@ -46,6 +50,213 @@ class _ClientDetailsScreenState extends State<ClientDetailsScreen> {
           },
         ),
       ),
+      bottomNavigationBar: BottomNavBar(
+        selectedIndex: 1,
+        onItemSelected: (index) {},
+        onItemTapped: (index) {},
+      ),
     );
   }
 }
+
+// Display the header.//
+class ClientHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CustomAppBar(
+      title: 'Client Name',
+      leftIcon: IconButton(
+        onPressed: () => Navigator.of(context).pop(),
+        icon: Icon(Icons.arrow_back, color: Color(0xFF0D121C)),
+        padding: EdgeInsets.zero,
+        ),
+      rightIcon: CurrencySwitcher(
+        initialUnit: DisplayUnit.dh,
+        onUnitChanged: (unit) {},
+      ),
+
+      );
+  }
+}
+
+// Display the contact info.//
+class ContactInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Text(
+            '456 Oak Ave, Anytown',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 16,
+              color: Color(0xFF0C8C96),
+              height: 21/16,
+            ),
+          ),
+          SizedBox(height: 4),
+          Text(
+            '0611224466',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              color: Color(0xFF0C8C96),
+              height: 21/18,
+              letterSpacing: 1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BalanceSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                '1,250.00',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF0D121C),
+                  height: 40/32,
+                ),
+              ),
+              SizedBox(width: 16),
+              Text(
+                'DH',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color(0xFF0D121C),
+                  height: 40/20,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Current Balance Due',
+            style: TextStyle(
+              fontSize: 16,
+              color: Color(0xFF0D121C),
+              height: 24/16,
+            ),
+          ),
+          SizedBox(height: 24),
+          SizedBox(
+            width: 210,
+            height: 56,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF1B3F77),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                elevation: 0,
+              ),
+              child: Text(
+                'Collect Payment',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  height: 21/14,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 24),
+        ],
+      ),
+    );
+  }
+}
+
+class TransactionsSection extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      date: "Oct 26, 2023, 10:30 AM",
+      type: "Delivery",
+      amount: "250.00",
+      currency: "DH",
+      isPositive: true,
+    ),
+    Transaction(
+      date: "Oct 25, 2023, 2:15 PM",
+      type: "Payment",
+      amount: "500.00",
+      currency: "DH",
+      isPositive: false,
+    ),
+    Transaction(
+      date: "Oct 24, 2023, 11:45 AM",
+      type: "Delivery",
+      amount: "1,500.00",
+      currency: "DH",
+      isPositive: true,
+    ),
+    Transaction(
+      date: "Oct 25, 2023, 2:15 PM",
+      type: "Payment",
+      amount: "500.00",
+      currency: "DH",
+      isPositive: false,
+    ),
+    Transaction(
+      date: "Oct 24, 2023, 11:45 AM",
+      type: "Delivery",
+      amount: "1,500.00",
+      currency: "DH",
+      isPositive: true,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              'Transactions',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF0D121C),
+                height: 23/18,
+              ),
+            ),
+          ),
+          Container(
+            constraints: BoxConstraints(maxHeight: 400),
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: transactions.length,
+              itemBuilder: (context, index) {
+                return TransactionItem(transaction: transactions[index]);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
